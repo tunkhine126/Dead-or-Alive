c60f5dec85bc499b0d99ae29c9db98d88d981c54
seerosecode <keshalakemusic@gmail.com>
2019-07-08 10:24:40 -0400

    deleted extra users controller file. all pertinent methods copied in current users controller file in api/v1

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,3 +35,21 @@ class Api::V1::UsersController < ApplicationController
   end
 
 end
+
+  # 
+  #
+  # def profile
+  #   @user = User.find(params[:id])
+  #   if @user
+  #     render json: {
+  #       id: @user.id,
+  #       username: @user.username,
+  #       img_url: @user.img_url,
+  #       zombie: @user.zombie,
+  #       quiz_id: @user.quiz_id,
+  #       location_id: @user.location_id
+  #     }
+  #   else
+  #     render json: {}, status: :not_found
+  #   end
+  # end
